ef3ad30cddf504cd169dafed5edc9cb3e7f78fc7
sjml2002 <sjml2002@naver.com>
2020-08-20 23:53:11 +0900

    소셜로그인, 일반로그인 디자인 완료

--- App/component/Login/Login.js
+++ App/component/Login/Login.js
@@ -1,5 +1,14 @@
 import React from "react";
-import { StyleSheet, Text, View, Button, Alert } from "react-native";
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  Button, 
+  Alert, 
+  ImageBackground,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 import { StatusBar } from "expo-status-bar";
 import * as GoogleSignIn from "expo-google-sign-in";
 import * as Facebook from "expo-facebook";
@@ -30,71 +39,136 @@ export default class Login extends React.Component {
     const { navigation } = this.props;
     return (
       <View style={styles.container}>
-        <Text>CAMPUS TAXI</Text>
-
-        <View style={styles.buttonContainer}>
-          <Button
-            title={
-              this.state.isWeb
-                ? "웹은 SNS를 지원하지 않습니다. 일반 로그인을 사용해주세요."
-                : "네이버 로그인"
-            }
-            onPress={this.naverLogin}
-            disabled={this.state.isWeb}
-          />
+      <ImageBackground 
+        source={require("../../../assets/background.png")}
+        style={styles.background}
+      >
+        {/* header */}
+        <View style={styles.header}>
+          <Image style={styles.logo} source={require("../../image/logo.png")}/>
+            <View style={{marginTop:10,}}>
+            <Text style={{color: "#ffffff", fontSize: 18}}>CAMPUS TAXI</Text>
+            </View>
         </View>
 
-        <View style={styles.buttonContainer}>
-          <Button
-            title={
-              this.state.isWeb
-                ? "웹은 SNS를 지원하지 않습니다. 일반 로그인을 사용해주세요."
-                : "카카오톡 로그인"
-            }
-            onPress={this.kakaoLogin}
-            disabled={this.state.isWeb}
-          />
+        {/* 소셜 로그인 */}
+        <View style={styles.body}>
+          <View style={[styles.buttonContainer, styles.naver_btn]}>
+            <TouchableOpacity
+              onPress={this.naverLogin}
+              disabled={this.state.isWeb}
+              style={{height: "100%",}}
+            >
+            <View style={styles.btn_content_container}>
+              <View style={styles.content_icon}>
+              <Image
+                style={{width: 25, height: 25,}}
+                source={require("../../../assets/icon_naver.png")}
+              />
+              </View>
+              <View style={styles.content_text}>
+              <Text style={{color: "#ffffff", fontSize: 18, textAlign: "center"}}> 
+                  { this.state.isWeb
+                    ? "웹은 SNS를 지원하지 않습니다. 일반 로그인을 사용해주세요."
+                    : "네이버 로그인" } </Text>
+              </View>
+            </View>
+            </TouchableOpacity>
+          </View>
+
+          <View style={[styles.buttonContainer, styles.kakao_btn]}>
+            <TouchableOpacity
+              onPress={this.kakaoLogin}
+              disabled={this.state.isWeb}
+              style={{height: "100%",}}
+            >
+              <View style={styles.btn_content_container}>
+                <View style={styles.content_icon}>
+                <Image
+                  style={{width: 25, height: 25,}}
+                  source={require("../../../assets/icon_kakao.png")}
+                />
+                </View>
+                <View style={styles.content_text}>
+                <Text style={{fontSize: 18, textAlign: "center"}}> 
+                    { this.state.isWeb
+                      ? "웹은 SNS를 지원하지 않습니다. 일반 로그인을 사용해주세요."
+                      : "카카오톡 로그인" } </Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          </View>
+
+          <View style={[styles.buttonContainer, styles.google_btn]}>
+            <TouchableOpacity
+              onPress={() => {
+                if (this.state.googleUser) {
+                  this.googleSignOutAsync();
+                } else {
+                  this.googleSignInAsync();
+                }
+              }}
+              disabled={this.state.isWeb}
+              style={{height: "100%",}}
+            >
+              <View style={styles.btn_content_container}>
+                <View style={styles.content_icon}>
+                <Image
+                  style={{width: 25, height: 25,}}
+                  source={require("../../../assets/icon_google.png")}
+                />
+                </View>
+                <View style={styles.content_text}>
+                <Text style={{color: "#ffffff", fontSize: 18, textAlign: "center"}}> 
+                    { this.state.isWeb
+                      ? "웹은 SNS를 지원하지 않습니다. 일반 로그인을 사용해주세요."
+                      : "구글 로그인" } </Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          </View>
+
+          <View style={[styles.buttonContainer, styles.facebook_btn]}>
+            <TouchableOpacity
+              onPress={this.fbLogIn}
+              disabled={this.state.isWeb}
+              style={{height: "100%"}}
+            >
+              <View style={styles.btn_content_container}>
+                <View style={styles.content_icon}>
+                <Image
+                  style={{width: 25, height: 25,}}
+                  source={require("../../../assets/icon_facebook.png")}
+                />
+                </View>
+                <View style={styles.content_text}>
+                <Text style={{color: "#ffffff", fontSize: 18, textAlign: "center"}}> 
+                    { this.state.isWeb
+                      ? "웹은 SNS를 지원하지 않습니다. 일반 로그인을 사용해주세요."
+                      : "페이스북 로그인" } </Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          </View>
+
+          <View style={[styles.buttonContainer, styles.normal_btn]}>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("로그인")}
+            >
+              <Text style={{textAlign: "center", color: "#ffffff", fontSize: 18, fontWeight:"bold",}}> 
+                일반 로그인 및 회원가입
+              </Text>
+            </TouchableOpacity>
+          </View>
+
         </View>
 
-        <View style={styles.buttonContainer}>
-          <Button
-            title={
-              this.state.isWeb
-                ? "웹은 SNS를 지원하지 않습니다. 일반 로그인을 사용해주세요."
-                : "구글 로그인"
-            }
-            onPress={() => {
-              if (this.state.googleUser) {
-                this.googleSignOutAsync();
-              } else {
-                this.googleSignInAsync();
-              }
-            }}
-            disabled={this.state.isWeb}
-          />
-        </View>
-
-        <View style={styles.buttonContainer}>
-          <Button
-            title={
-              this.state.isWeb
-                ? "웹은 SNS를 지원하지 않습니다. 일반 로그인을 사용해주세요."
-                : "페이스북 로그인"
-            }
-            onPress={this.fbLogIn}
-            disabled={this.state.isWeb}
-          />
-        </View>
-
-        <View style={styles.buttonContainer}>
-          <Button
-            title="일반 로그인 및 회원가입"
-            onPress={() => navigation.navigate("로그인")}
-          />
-        </View>
         <StatusBar style="auto" />
+      </ImageBackground>
       </View>
     );
+
+
     moveToLoginScreen = () => {
       navigation.navigate("로그인");
     };
@@ -205,6 +279,9 @@ export default class Login extends React.Component {
   };
 }
 
+
+
+///////// 스타일 ///////////
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -212,10 +289,68 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-
   buttonContainer: {
+    flex: 1,
     width: "60%",
     maxWidth: 400,
-    marginVertical: 10,
+    marginBottom: 30,
+    height: 50,
+  },
+  logo: {
+    width: 91,
+    height: 82,
+  },
+  background: {
+    width: "100%",
+    height: "100%",
+  },
+  header: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  body: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+
+  //각 소셜 로그인 버튼 styles
+  naver_btn: {
+    backgroundColor: "#1EC700",
+    borderRadius: 30,
+  },
+  kakao_btn: {
+    backgroundColor: "#FDEC00",
+    borderRadius: 30,
+  },
+  google_btn: {
+    backgroundColor: "#CC3731",
+    borderRadius: 30,
+  },
+  facebook_btn: {
+    backgroundColor: "#3B5999",
+    borderRadius: 30,
+  },
+  normal_btn: {
+
+  },
+
+  //버튼 안 내용 (아이콘, 텍스트)
+  btn_content_container: {
+    flex:1,
+    flexDirection: "row",
+    height: "100%",
+  },
+  content_icon: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 5,
+  },
+  content_text: {
+    flex: 6, 
+    alignSelf:"center", 
+    justifyContent:"center"
   },
 });
--- App/component/Login/LoginScreen.js
+++ App/component/Login/LoginScreen.js
@@ -1,6 +1,15 @@
 import * as React from "react";
 import { Component, useState } from "react";
-import { Button, View, Text, TouchableOpacity, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Button, 
+  View,
+  Text, 
+  TouchableOpacity, 
+  TextInput,
+  ImageBackground,
+  Image,
+} from "react-native";
 import { bbsStore, userStore } from "store";
 
 export default class LoginScreen extends Component {
@@ -18,51 +27,164 @@ export default class LoginScreen extends Component {
       });
     }
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>아이디와 비밀번호를 입력해주세요.</Text>
-        <TextInput
-          placeholder="아이디를 입력해주세요"
-          style={{
-            fontSize: 18,
-            margin: 0,
-            borderColor: "#27BE5E",
-            borderBottomWidth: 2,
-            width: "60%",
-          }}
-          value={this.state.id}
-          onChangeText={(textEntry) => this.setState({ id: textEntry })}
-          onSubmitEditing={() => login(this.state.id, this.state.pw)}
-        />
-        <TextInput
-          placeholder="비밀번호을 입력해주세요"
-          style={{
-            fontSize: 18,
-            margin: 0,
-            borderColor: "#27BE5E",
-            borderBottomWidth: 2,
-            width: "60%",
-          }}
-          value={this.state.pw}
-          onChangeText={(textEntry) => this.setState({ pw: textEntry })}
-          onSubmitEditing={() => login(this.state.id, this.state.pw)}
-        />
-        <Button
-          title="로그인하기"
-          onPress={() => login(this.state.id, this.state.pw)}
-        />
-        <Button
-          title="아이디찾기"
-          onPress={() => navigation.navigate("아이디 찾기")}
-        />
-        <Button
-          title="비밀번호 찾기"
-          onPress={() => navigation.navigate("비밀번호 찾기")}
-        />
-        <Button
-          title="회원가입"
-          onPress={() => navigation.navigate("이용동의", { token: null })}
-        />
+      <View style={LoginStyle.container}>
+      <ImageBackground 
+        source={require("../../../assets/background.png")}
+        style={LoginStyle.background}
+      >
+        {/* header */}
+        <View style={LoginStyle.header}>
+          <Image style={LoginStyle.logo} source={require("../../image/logo.png")}/>
+            <View style={{marginTop:10,}}>
+            <Text style={{color: "#ffffff", fontSize: 18}}>CAMPUS TAXI</Text>
+            </View>
+        </View>
+
+        <View style={LoginStyle.body}>
+          <View style={LoginStyle.login_input_container}>
+            <TextInput
+              placeholder="아이디를 입력해주세요"
+              placeholderTextColor="#f0f0f0"
+              style={LoginStyle.login_input_text}
+              value={this.state.id}
+              onChangeText={(textEntry) => this.setState({ id: textEntry })}
+              onSubmitEditing={() => login(this.state.id, this.state.pw)}
+            />
+            <TextInput
+              placeholder="비밀번호을 입력해주세요"
+              placeholderTextColor="#f0f0f0"
+              style={LoginStyle.login_input_text}
+              value={this.state.pw}
+              onChangeText={(textEntry) => this.setState({ pw: textEntry })}
+              onSubmitEditing={() => login(this.state.id, this.state.pw)}
+              secureTextEntry
+            />
+          </View>
+
+          <View style={{flexDirection: "row", width: "60%", marginBottom:40,}}>
+            <View style={{flex: 2, alignItems: "flex-end"}}>
+            <TouchableOpacity 
+              onPress={() => navigation.navigate("아이디 찾기")} 
+            >
+              <Text style={{color: "#f0f0f0"}}> 아이디 찾기 </Text>
+            </TouchableOpacity>
+            </View>
+
+            <View style={{flex:1, alignItems:"center"}}>
+            <Text style={{color: "#f0f0f0", fontWeight: "bold"}}> | </Text>
+            </View>
+
+            <View style={{flex: 2, alignItems: "flex-start"}}>
+            <TouchableOpacity 
+              onPress={() => navigation.navigate("비밀번호 찾기")} 
+            >
+              <Text style={{color: "#f0f0f0"}}> 비밀번호 찾기 </Text>
+            </TouchableOpacity>
+            </View>
+          </View>
+
+          <View style={LoginStyle.button_container}>
+            <View style={[LoginStyle.login_btn_style, LoginStyle.login_btn]}>
+              <TouchableOpacity
+                onPress={() => login(this.state.id, this.state.pw)}
+                style={{height: "100%"}}
+              >
+                <View style={LoginStyle.login_text}>
+                  <Text style={{fontSize: 18, fontWeight: "bold"}}> 
+                    로그인하기 
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+            
+            <View style={LoginStyle.login_btn_style}>
+              <TouchableOpacity
+                onPress={() => navigation.navigate("이용동의", { token: null })}
+              >
+                <Text style={{textAlign: "center", color: "#ffffff", fontSize: 18, fontWeight:"bold",}}> 
+                  회원가입
+                </Text>
+              </TouchableOpacity>
+            </View>
+
+          </View>
+        </View>
+      
+      </ImageBackground>
       </View>
     );
   }
 }
+
+
+//onPress={() => login(this.state.id, this.state.pw)}
+
+
+
+
+
+////// 스타일 //////
+const LoginStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  background: {
+    width: "100%",
+    height: "100%",
+  },
+  //로고, 캠퍼스택시 container
+  header: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    width: 91,
+    height: 82,
+  },
+  //로그인 body container
+  body: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  login_input_container: {
+    width: "60%",
+    marginBottom: 10,
+  },
+  login_input_text: {
+    fontSize: 18,
+    margin: 0,
+    borderColor: "#a9a9a9",
+    borderBottomWidth: 2,
+    marginBottom: 20,
+    paddingBottom: 10,
+    textAlign: "center",
+    color: "#ffffff",
+  },
+  button_container: {
+    flex: 1,
+    width: "60%",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+
+  login_btn_style: {
+    width: "100%",
+    maxWidth: 400,
+    marginBottom: 30,
+    height: 50,
+  },
+  login_btn: {
+    backgroundColor: "#EDEDED",
+    borderRadius: 30,
+  },
+  login_text: {
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+})
