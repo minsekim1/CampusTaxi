26fc82b1326d2020bb5461ffb3abd2c05fb1ee28
Mins97 <tkarnrwl78627862@gmail.com>
2020-11-15 06:55:27 +0900

    a

--- App/component/Email/EmailComposer.js
+++
@@ -1,9 +0,0 @@
-import * as MailComposer from "expo-mail-composer";
-
-export default async function sendEmailAsync(subject_text, body_text) {
-  let result = await MailComposer.composeAsync({
-    recipients: ["campustaxi@naver.com"],
-    subject: subject_text,
-    body: body_text, //"사용하시는 기종이 무엇입니까? \n결론이 무엇인가요? \n구체적으로 설명부탁드립니다. : "
-  });
-}
