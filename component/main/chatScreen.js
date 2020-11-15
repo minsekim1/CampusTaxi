344c7f6d7ee8297c8eb373d417178188d2eb4766
Mins97 <tkarnrwl78627862@gmail.com>
2020-11-16 04:37:16 +0900

    b

---
+++ component/main/chatScreen.js
@@ -0,0 +1,185 @@
+//채팅목록 화면
+export default function chatScreen({ route, navigation }) {
+  const bbstype = route.params.bbstype;
+  const myname = userStore.user.name;
+  const mygender = 1; //userStore.user.d;
+  const [bbslist, setBbslist] = useState([{ "cost": 1, "bbsid": 1, "gender": 1, "bbsDate": "2020-11-15 00:09:46.000000", "bbstype": 1, "endplace": "endplace 1", "available": 1, "personmax": 1, "leadername": "minsekim", "startplace": "startplace 1", "meetingdate": "2020-11-15 00:09:46.000000", "personmember": "personmember 1", "personpresent": 1 }
+    , { "cost": 2, "bbsid": 2, "gender": 2, "bbsDate": "2020-11-15 00:09:46.000000", "bbstype": 1, "endplace": "endplace 2", "available": 1, "personmax": 2, "leadername": "ohju", "startplace": "startplace 2", "meetingdate": "2020-11-15 00:09:46.000000", "personmember": "personmember 2", "personpresent": 2 }
+    , { "cost": 3, "bbsid": 3, "gender": 0, "bbsDate": "2020-11-15 00:09:46.000000", "bbstype": 2, "endplace": "endplace 3", "available": 1, "personmax": 3, "leadername": "ohju", "startplace": "startplace 3", "meetingdate": "2020-11-15 00:09:46.000000", "personmember": "personmember 3", "personpresent": 3 }]
+  );
+  React.useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      userStore.login("loginid 1", "loginpassword 1");
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const style = {
+    header: {
+      h: { height: 80 },
+      leftComponent: <Button
+        type="clear"
+        title=""
+        icon={<Icon name="arrow-back" size={24} color="white" />}
+        onPress={() => navigation.goBack()}
+      ></Button>,
+      centerComponent: {
+        text: bbstype + " 채팅방 목록",
+        style: campusStyle.Text.middleBold,
+      },
+      rightComponent: <Button
+        type="clear"
+        title=""
+        icon={<Icon name="refresh" size={24} color="white" />}
+        onPress={() => userStore.getBbsType(bbstype).then(r => setBbslist(r))}
+      ></Button>,
+    },
+    container: {
+      flex: 1,
+      marginTop: StatusBar.currentHeight || 0,
+    },
+    touch: {
+      backgroundColor: "white",
+      padding: 10,
+    },
+  };
+  async function enter(bbs) {
+    // let isEnter = fetch?
+    let isEnter = await userStore.isEnter(bbs);
+    if (isEnter) {
+      navigation.navigate("채팅방", {
+        bbs: bbs,
+        mygender: mygender,
+        myname: myname,
+      });
+    }
+  }
+  //#endregion
+  const renderItem = ({ item }) => <Item item={item} />;
+  const Item = ({ item }) => (
+    <TouchableOpacity onPress={() => enter(item)} style={style.touch}>
+      <View style={campusStyle.View.row}>
+        <View
+          style={{
+            borderRadius: 100,
+            width: 55,
+            height: 55,
+            backgroundColor:
+              item.gender == 0
+                ? "#579FEE"
+                : item.gender == 1
+                  ? "#C278DE"
+                  : "#3A3A3A",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text style={campusStyle.Text.middleBold}>
+            {item.bbsid}
+          </Text>
+          <Text style={campusStyle.Text.middleBold}>
+            {item.gender == 0
+              ? "남자"
+              : item.gender == 1
+                ? "여자"
+                : "모두"}
+          </Text>
+        </View>
+        <View style={{ flex: 6 }}>
+          <View style={campusStyle.View.row}>
+            <Image
+              style={{
+                width: 23,
+                height: 15,
+                marginLeft: 10,
+              }}
+              source={crown}
+            />
+            <Text>{(String(item.leadername).length > 9) ? String(item.leadername).substring(0, 9) + "..." : item.leadername}</Text>
+          </View>
+          <Text style={{ marginLeft: 10 }}>
+            출발지:{item.startplace}
+          </Text>
+          <Text style={{ marginLeft: 10 }}>
+            도착지:{item.endplace}
+          </Text>
+        </View>
+        <View style={{ flex: 1 }}>
+          {
+            item.personpresent >= item.personmax ?
+              <Text style={campusStyle.Text.red}>
+                {item.personpresent}/{item.personmax}
+              </Text>
+              :
+              <Text>
+                {item.personpresent}/{item.personmax}
+              </Text>
+          }
+        </View>
+        <View style={{ flex: 3, alignItems: "center" }}>
+          <Text style={campusStyle.Text.grayDDark}>
+            탑승시간▼
+          </Text>
+          <Text style={campusStyle.Text.grayDDark}>
+            {item.meetingdate}
+          </Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+  return (
+    <>
+      <Header
+        containerStyle={style.header.h}
+        backgroundColor="#0D3664"
+        leftComponent={style.header.leftComponent}
+        centerComponent={style.header.centerComponent}
+        rightComponent={style.header.rightComponent}
+      />
+      {/* 채팅목록 출력부분 */}
+      <FlatList data={bbslist} renderItem={renderItem} keyExtractor={item => String(item.bbsid)} />
+      {/* 방만들기 버튼부분 */}
+      <View style={campusStyle.View.createRoomView}>
+        <TouchableOpacity
+          style={campusStyle.Button.createRoomBtn}
+          onPress={() =>
+            navigation.navigate("방 만들기", {
+              bbstype: bbstype,
+            })
+          }
+        >
+          <Icon name="add" size={32} color="white" />
+          <Text style={campusStyle.Text.smallSize}>방만들기</Text>
+        </TouchableOpacity>
+      </View>
+    </>
+  );
+}
+
+
+//#region imports
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  FlatList,
+  Image,
+  TouchableOpacity,
+  ScrollView, SafeAreaView, StatusBar
+} from "react-native";
+// import * as TimeAPI from "../Email/globalTimeAPI.js";
+import {
+  Header,
+  ListItem,
+  Icon,
+  Text,
+  Card,
+  Button,
+  ButtonGroup,
+  Input,
+} from "react-native-elements";
+import campusStyle from "./campusStyle";
+import crown from "./image/crown.png";
+import { userStore } from "../store/store";
+
+//#endregion
---
+++ component/main/createRoom.js
@@ -0,0 +1,199 @@
+import React, { useState } from "react";
+import { View } from "react-native";
+import { Text, Button, ButtonGroup } from "react-native-elements";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { Picker } from "@react-native-community/picker";
+import campusStyle from "./campusStyle";
+import { userStore } from "store";
+
+export default function createRoom({ route, navigation }) {
+  const bbstype = route.params.bbstype;
+  const [createRoomCategory, setCreateRoomCategory] = useState(bbstype);
+  const [createRoompersonmax, setCreateRoompersonmax] = useState(4);
+  const [createRoomGender, setCreateRoomGender] = useState(1);
+  const [createSelectGender, setCreateSelectGender] = useState(2);
+  const [placeStart, setPlaceStart] = useState(null);
+  const [placeEnd, setPlaceEnd] = useState(null);
+  //채팅방만들기시간선택
+  const [date, setDate] = useState(new Date());
+  const [mode, setMode] = useState("date");
+  const [show, setShow] = useState(false);
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === "ios");
+    setDate(currentDate);
+  };
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+  const menuList = [
+    "등교",
+    "하교",
+    "야작",
+    "독서실",
+    "PC방",
+    "놀이동산",
+    "클럽",
+    "스키장",
+    "오션월드",
+  ];
+  const showDatepicker = () => showMode("date");
+  const showTimepicker = () => showMode("time");
+  return (
+    <>
+      <View style={{ padding: 20 }}>
+        <Text>카테고리</Text>
+        <Picker
+          selectedValue={createRoomCategory}
+          onValueChange={(itemValue) => {
+            setCreateRoomCategory(itemValue);
+          }}
+        >
+          <Picker.Item key={"gray"} color="gray" label={bbstype} />
+          {menuList.map((item) =>
+            item != bbstype ? (
+              <Picker.Item
+                key={(item, i) => String(i)}
+                label={item}
+                value={item}
+              />
+            ) : null
+          )}
+        </Picker>
+
+        {placeStart != null && placeEnd != null ? (
+          <>
+            <Text>출발지:{placeStart.name}</Text>
+            <Text>도착지:{placeEnd.name}</Text>
+          </>
+        ) : (
+            <Text>출발지 / 도착지를 선택해주세요.</Text>
+          )
+        }
+
+        <Button
+          style={campusStyle.Button.default}
+          title={
+            placeStart == null || placeEnd == null
+              ? "이곳에서 출발지/도착지를 선택해주세요."
+              : "출발지/도착지 재입력하기"
+          }
+          onPress={() => {
+            navigation.navigate("지도검색");
+          }}
+        />
+        <Text>탑승 시간</Text>
+        <Text style={campusStyle.Text.center}>
+          {userStore.tokoreanTime(date)}
+        </Text>
+        <View style={campusStyle.View.row}>
+          <View style={campusStyle.View.flex}>
+            <Button
+              buttonStyle={campusStyle.Button.marginTopAndBottom5}
+              onPress={showDatepicker}
+              title="날짜 선택"
+            />
+          </View>
+          <View style={campusStyle.View.flex}>
+            <Button
+              buttonStyle={campusStyle.Button.marginTopAndBottom5}
+              onPress={showTimepicker}
+              title="시간 선택"
+            />
+          </View>
+        </View>
+        {/* 날짜선택 Picker창 */}
+        {show && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={date}
+            mode={mode}
+            is24Hour={true}
+            display="default"
+            onChange={onChange}
+          />
+        )}
+        <Text>탑승 인원</Text>
+        <ButtonGroup
+          selectedButtonStyle={{ backgroundColor: "#F8BD3C" }}
+          textStyle={{ fontSize: 20 }}
+          selectedIndex={createRoompersonmax - 2}
+          onPress={(index) => setCreateRoompersonmax(index + 2)}
+          buttons={["2", "3", "4"]}
+        ></ButtonGroup>
+        <Text>입장 제한 성별</Text>
+        <ButtonGroup
+          selectedButtonStyle={{ backgroundColor: "#F8BD3C" }}
+          textStyle={{ fontSize: 16 }}
+          selectedIndex={createRoomGender}
+          onPress={(index) => {
+            if (index == 0) {
+              setCreateRoomGender(0);
+              setCreateSelectGender(Number(userStore.user.gender));
+            } else {
+              setCreateRoomGender(1);
+              setCreateSelectGender(2);
+            }
+          }}
+          buttons={["동성만", "남녀모두"]}
+        ></ButtonGroup>
+      </View>
+      <View style={campusStyle.View.rowflexBtnGroup}>
+        <Button
+          name="myButtonName"
+          buttonStyle={campusStyle.Button.groupActive}
+          title="방 생성"
+          onPress={() => {
+            if (
+              placeEnd == null ||
+              placeStart == null
+            ) {
+              alert("출발지 또는 도착지를 선택해주세요.");
+            } else {
+              userStore.isCreate(bbstype)
+                .then(r => {
+                  if (r) {
+                    //REST API 3 - 2. createRoom.js isCreate
+                    //fetch(<EC2:url>/bbs/iscreate/userid/2/bbstype/1 => return true/false)
+                    // * if조건: isEnter과 비슷하게 카테고리(bbsytype)별로 1개씩만 만들 수 있음
+                    alert("방이 생성되었습니다. 내 채팅에서 확인해주세요.");
+                    // bbsStore
+                    //   .addBbs(
+                    //     createRoomCategory,
+                    //     anotherStore.placeEnd,
+                    //     createSelectGender,
+                    //     userStore.user.i,
+                    //     date,
+                    //     createRoompersonmax,
+                    //     anotherStore.placeStart,
+                    //     userStore.userkey
+                    //   )
+                    //   .then(() => {
+                    //   });
+                  } else {
+                    alert(
+                      "채팅방은 카테고리별로 1개만 들어갈 수 있습니다. 내 채팅->채팅방->사람아이콘 클릭에서 채팅방 나가기를 해주세요."
+                    );
+                  }
+                  setPlaceEnd(null);
+                  setPlaceStart(null);
+                  navigation.goBack();
+                });
+            }
+          }}
+        />
+        <Button
+          name="myButtonName"
+          buttonStyle={campusStyle.Button.groupCancel}
+          title="취소"
+          onPress={() => {
+            setPlaceEnd(null);
+            setPlaceStart(null);
+            navigation.goBack();
+          }}
+        />
+      </View>
+    </>
+  );
+}
