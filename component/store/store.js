3c4d522d987478f6534df3f6609acb0fd678556f
Mins97 <tkarnrwl78627862@gmail.com>
2020-11-16 04:37:01 +0900

    a

--- component/store/store.js
+++
@@ -1,7 +0,0 @@
-//MobX
-import UserStore from "./UserStore";
-//전역 Store
-//import { userStore } from "store";
-//<Button onPress={() => userStore.printUserStore()} title="유저 출력"/>
-
-export const userStore = new UserStore();
