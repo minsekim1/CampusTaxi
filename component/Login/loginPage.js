90a4271d0e22622043df8a219392070437ec892c
Mins97 <tkarnrwl7862@naver.com>
2020-12-05 00:10:22 +0900

    autoLogin add

--- component/Login/loginPage.js
+++ component/Login/loginPage.js
@@ -1,3 +1,4 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as React from "react";
 import { Component, useState } from "react";
 import {
@@ -7,19 +8,41 @@ import {
   Text,
   TouchableOpacity,
   TextInput,
-  ImageBackground,
+  ImageBackground,Switch
 } from "react-native";
+import { Button  } from "react-native-paper";
 import { userStore } from "../store/store";
 import { AuthContext } from "../store/UserStore"
 import LogoWhite from "./logoWhite.js";
 export default function loginPage({ route, navigation }) {
-  const [id, setId] = useState("");
-  const [pw, setPw] = useState("");
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+  const [autoLogin, setAutoLogin] = useState(false);
   const { signIn } = React.useContext(AuthContext);
   async function login() {
     if (id != "" && pw != "")
-      await userStore.login(id, pw).then((r) => { if (r !== null) { signIn({ id, pw }) } });
+      await userStore.login(id, pw).then((r) => 
+      { if (r !== null) {
+        if(autoLogin){
+          AsyncStorage.setItem('id',id); 
+          AsyncStorage.setItem('pw',pw);
+          AsyncStorage.setItem('autoLogin','true');
+        }else{
+          AsyncStorage.setItem('id',''); 
+          AsyncStorage.setItem('pw','');
+          AsyncStorage.setItem('autoLogin','false');
+        }
+        signIn({ id, pw }); } });
   }
+  React.useEffect(()=>{
+    AsyncStorage.getItem('autoLogin').then((r)=>{
+      if(r){
+        AsyncStorage.getItem('id').then((r)=>setId(r));
+        AsyncStorage.getItem('pw').then((r)=>setPw(r));
+        AsyncStorage.getItem('autoLogin').then((r)=>{if(r=='true')setAutoLogin(true);else setAutoLogin(false)});
+      }
+    })
+  },[])
   return (
     <View style={LoginStyle.container}>
       <ImageBackground
@@ -53,6 +76,11 @@ export default function loginPage({ route, navigation }) {
               onSubmitEditing={() => login()}
               secureTextEntry
             />
+            <View style={{flexDirection:"row", alignItems:"center",justifyContent:"flex-end"}}>
+            <Switch
+                    onValueChange={(r)=>setAutoLogin(r)}
+                    value={autoLogin}
+            /><Text style={{color:"white"}}>자동 로그인</Text></View>
           </View>
           <View style={LoginStyle.button_container}>
             <View style={[LoginStyle.login_btn_style, LoginStyle.login_btn]}>
